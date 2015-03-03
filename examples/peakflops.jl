b1ad356e1eb989b03f2af4f24223be9dd732f4e9
jake bolewski <jakebolewski@gmail.com>
2015-03-03 14:09:14 -0500

    add simple peakflops example (not working)

---
+++ examples/peakflops.jl
@@ -0,0 +1,18 @@
+import PAPI
+
+function main(ctrs...)
+    cs = PAPI.CounterSet([PAPI.TOT_INS])
+ 
+    PAPI.start_counters(cs)
+    start = PAPI.read_counters!(cs)
+
+    peakflops()
+
+    stop = PAPI.accum_counters!(cs)
+    PAPI.stop_counters(cs)
+    
+    ninst = Int(stop[1]) - Int(stop[1])
+    println("Computed peakflops in $(ninst) instructions")
+end
+
+main()
