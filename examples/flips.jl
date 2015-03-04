39f41eb5d42b92727b59950c00619be2e1a107ee
jake bolewski <jakebolewski@gmail.com>
2015-03-03 19:02:09 -0500

    add flips example

---
+++ examples/flips.jl
@@ -0,0 +1,17 @@
+import PAPI
+
+function slow_code()
+    tmp = 1.1
+    for i=1:200_000_000
+        tmp = (tmp+100)/i
+    end
+    return tmp
+end
+
+function driver()
+    real_time, proc_time, flpins, mflips = 
+        @PAPI.flips slow_code()
+    @printf("Real_time: %f Proc_time: %f Total flpins: %lld MFLIPS: %f\n", 
+             real_time, proc_time, flpins, mflips)
+end
+driver()
